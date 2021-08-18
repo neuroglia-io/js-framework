e9cadb7b6d5966185e4d9c23344c894fcd7b8326
JBBianchi <jb.bianchi@neuroglia.io>
2021-08-18 12:08:09 +0200

    @neuroglia/angular-common - exported camel/human/kebab/pascal/snake case pipes

--- projects/neuroglia/angular-common/src/lib/index.ts
+++ projects/neuroglia/angular-common/src/lib/index.ts
@@ -4,3 +4,9 @@ export * from './application-loading.service';
 export * from './dom-offset.service';
 export * from './storage-handler-factory.service';
 export * from './uri-component.query-encoder';
+export * from './pipes/camel-case.pipe';
+export * from './pipes/human-case.pipe';
+export * from './pipes/kebab-case.pipe';
+export * from './pipes/pascal-case.pipe';
+export * from './pipes/snake-case.pipe';
+
