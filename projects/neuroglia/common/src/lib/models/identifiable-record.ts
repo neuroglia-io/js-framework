e23ff8eca457b3b2650edba4d29da86dbe0a4705
JBBianchi <jb.bianchi@neuroglia.io>
2023-11-29 16:28:52 +0100

    fix(common): enabled hydration of IdentifiableRecord

--- projects/neuroglia/common/src/lib/models/identifiable-record.ts
+++ projects/neuroglia/common/src/lib/models/identifiable-record.ts
@@ -1,7 +1,12 @@
+import { ModelConstructor } from './model-constructor';
+
 /**
  * Represents the default abstract implementation of the identifiable record/object
  */
-export class IdentifiableRecord<T = string> {
+export class IdentifiableRecord<T = string> extends ModelConstructor {
+  constructor(model?: any) {
+    super(model);
+  }
   /** The object's unique identifier */
   id: T;
 }
