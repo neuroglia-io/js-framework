95debf1c34c0f73b42fb7df8a902fee787219557
JBBianchi <jb.bianchi@neuroglia.io>
2023-12-12 12:35:37 +0100

    fix(angular-application-common): changed keycloak injection methodology

--- projects/neuroglia/angular-application-common/src/lib/startup-application.ts
+++ projects/neuroglia/angular-application-common/src/lib/startup-application.ts
@@ -1,37 +1,54 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { EnvironmentProviders, Provider, enableProdMode } from '@angular/core';
-import { KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
+import { APP_INITIALIZER, EnvironmentProviders, Provider, enableProdMode, importProvidersFrom } from '@angular/core';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { IApplicationConfig } from './models';
 import { APPLICATION_CONFIG_TOKEN } from './providers';
 import { defaultKeycloakInitOptions } from './default-keycloak-options';
 
-// get the application baseUrl
+/** Gets the application baseUrl */
 function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 
-// a KeycloakService instance created manually and provided to the app
-const keycloakService = new KeycloakService();
+/** Basic providers for BASE_URL. The array will be extented in the startup process */
+const providers: Array<Provider | EnvironmentProviders> = [{ provide: 'BASE_URL', useFactory: getBaseUrl }];
 
-// basic providers for BASE_URL and Keycloak. The application config token will be added later in the process
-const providers: Array<Provider | EnvironmentProviders> = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl },
-  { provide: KeycloakService, useValue: keycloakService },
-  { provide: HTTP_INTERCEPTORS, useClass: KeycloakBearerInterceptor, multi: true },
-];
-
+/** The factory used to initialize the keycloak service */
+const initKeycloak = (config: IApplicationConfig, keycloak: KeycloakService) => () =>
+  keycloak
+    .init({
+      ...config.keycloakOptions,
+      initOptions: {
+        ...defaultKeycloakInitOptions,
+        enableLogging: config.environment === 'development',
+        ...(config.keycloakOptions.initOptions || {}),
+      },
+    })
+    .then((authenticated: boolean) => {
+      if (authenticated) {
+        return Promise.resolve();
+      }
+      return keycloak.login({ redirectUri: window.location.href }).then(() => Promise.reject());
+    });
 /**
  * Startup the application with the provided configuration file
  * @param applicationConfigFile The configuration file URL to start the application with
+ * @param extraConfig Some extra configuration, useful for non serializable members
  * @returns A promise of providers (@see {@link Provider} and @see {@link EnvironmentProviders}) containing the application config token and keycloak services/interceptors
  */
-export const startupApplication = (applicationConfigFile: string): Promise<Array<Provider | EnvironmentProviders>> =>
+export const startupApplication = (
+  applicationConfigFile: string,
+  extraConfig: Partial<IApplicationConfig> | undefined = undefined,
+): Promise<Array<Provider | EnvironmentProviders>> =>
   fetch(applicationConfigFile)
     .then((res) => {
       if (res.status === 200) return res.json();
       throw new Error('Failed to fetch application configuration.');
     })
     .then((config: IApplicationConfig) => {
+      config = {
+        ...config,
+        ...(extraConfig || {}),
+      };
       if (!config?.environment || !config?.keycloakOptions?.config) {
         throw new Error('The configuration seems to be empty or invalid');
       }
@@ -39,22 +56,12 @@ export const startupApplication = (applicationConfigFile: string): Promise<Array
         enableProdMode();
       }
       providers.push({ provide: APPLICATION_CONFIG_TOKEN, useValue: config });
-      return keycloakService.init({
-        ...config.keycloakOptions,
-        initOptions: {
-          ...defaultKeycloakInitOptions,
-          enableLogging: config.environment === 'development',
-          ...(config.keycloakOptions.initOptions || {}),
-        },
+      providers.push(importProvidersFrom([KeycloakAngularModule]));
+      providers.push({
+        provide: APP_INITIALIZER,
+        useFactory: initKeycloak,
+        multi: true,
+        deps: [APPLICATION_CONFIG_TOKEN, KeycloakService],
       });
-    })
-    .then((authenticated: boolean) => {
-      if (authenticated) {
-        return Promise.resolve(providers);
-      }
-      return keycloakService
-        .login({
-          redirectUri: window.location.href,
-        })
-        .then(() => Promise.reject('Unauthenticated'));
+      return Promise.resolve(providers);
     });
