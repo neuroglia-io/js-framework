f6c415bc7daaa7d92bf37573352a40b63c42b0e4
JBBianchi <jb.bianchi@neuroglia.io>
2023-12-18 17:56:25 +0100

    fix(integration): fixed aggregate version

--- projects/neuroglia/integration/src/lib/integration-event.interface.ts
+++ projects/neuroglia/integration/src/lib/integration-event.interface.ts
@@ -7,7 +7,7 @@ export interface IIntegrationEvent<T = string> {
   /** The id of the aggregate, if any, that has produced the event */
   aggregateId: T;
   /** The version of the event's source aggregate, if any, at the time it produced the event */
-  aggregateVersion: string;
+  aggregateVersion: number;
   /** The type of the originating intergration event */
   type: string;
 }
